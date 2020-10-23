00931bfb5daa4336d14713a4aed2b80af06feac5
nareshkumarkatta9@gmail.com <nareshkumarkatta9@gmail.com>
2020-10-23 14:25:32 +0530

    Add project files.

---
+++ MyFirstAndroidApp/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace MyFirstAndroidApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        TextView txtNumber;
+        int number;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+             txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+             txtNumber.Text = (++number).ToString();
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
+             txtNumber.Text = (--number).ToString();
+        }
+    }
+}
